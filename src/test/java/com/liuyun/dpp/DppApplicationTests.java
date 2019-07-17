32ceed0091b476e43a3edce8d029df3b1a60afca
duotong.hu <duotong.hu@hualongdata.com>
2019-07-17 10:46:18 +0800

    提交信息

---
+++ src/test/java/com/liuyun/dpp/DppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.liuyun.dpp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DppApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
